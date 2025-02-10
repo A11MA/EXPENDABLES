762b67677fa87774f966834d9c289404ba5282a0
Valentin <valentinas.daujotas@gmail.com>
2025-02-10 15:41:27 +0200

    abaut header ir contact

---
+++ images/nav_bar.js
@@ -0,0 +1,11 @@
+// burger meniu funkcionalumas.
+
+
+function toggleMenu() {
+    let menu = document.getElementById("mobileMenu");
+    if (menu.style.right === "0px") {
+        menu.style.right = "-250px";
+    } else {
+        menu.style.right = "0px";
+    }
+}
